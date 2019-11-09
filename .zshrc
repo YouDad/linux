8b08f4b93b95eecf0303f6844d88e10db21a4d3e
YouDad <1632083718@qq.com>
2019-11-09 18:08:56 +0800

    Add hhistory

--- .zshrc
+++ .zshrc
@@ -148,3 +148,35 @@ if [[ "$?" == "0" ]]; then
 	alias cls="colorls --sd"
 fi
 export PATH="$HOME/My/src/nodejs/bin:$PATH"
+
+# CTRL-R - Paste the selected command from history into the command line
+while [ -e $HOME/bin/history.cpp ]; do
+	if [ ! -e $HOME/bin/hhistory ]; then
+		g++ -v 1>/dev/null 2>&1
+		if [[ $? == 0 ]]; then
+			g++ $HOME/bin/history.cpp -g -o $HOME/bin/hhistory
+		else
+			echo "please install g++"
+			break
+		fi
+	fi
+
+	fzf-history-widget() {
+		local selected num
+		setopt localoptions noglobsubst noposixbuiltins pipefail 2> /dev/null
+		selected=( $(a.out ~/.zsh_history |
+			FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort $FZF_CTRL_R_OPTS --query=${(qqq)LBUFFER} +m" $(__fzfcmd)) )
+		local ret=$?
+		if [ -n "$selected" ]; then
+			num=$selected[1]
+			if [ -n "$num" ]; then
+				zle vi-fetch-history -n $num
+			fi
+		fi
+		zle reset-prompt
+		return $ret
+	}
+	zle     -N   fzf-history-widget
+	bindkey '^R' fzf-history-widget
+	break
+done
---
+++ bin/history.cpp
@@ -0,0 +1,151 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <vector>
+#include <string>
+
+std::vector<std::pair<int, std::string>>v;
+
+struct YMDHMS {
+	int Y, M, D, h, m, s;
+};
+
+void fast_date(struct YMDHMS *date, int ts)
+{
+	ts += 8 * 3600;
+	date->h = ts % 86400 / 3600;
+	date->m = ts % 3600 / 60;
+	date->s = ts % 60;
+	date->D = ts / 86400 + 365;
+	int Y4 = date->D / (365 * 4 + 1);
+	date->D -= Y4 * (365 * 4 + 1);
+	date->Y = date->D / 365;
+	date->Y = date->Y == 4 ? 3 : date->Y;
+	date->D -= date->Y * 365;
+
+	if (date->Y == 3 && date->D == 59) {
+		date->M = 2;
+		date->D = 28;
+	} else if (date->Y == 3 && date->D > 59) {
+		date->D--;
+	}
+
+	int months[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	int i;
+
+	for (i = 0; date->D >= months[i]; i++) {
+		date->D -= months[i];
+	}
+
+	date->M = i + 1;
+	date->D++;
+	date->Y += Y4 * 4 + 1969;
+}
+
+int main(int argc, char **argv)
+{
+	FILE *fp = NULL;
+	if (argc != 2) {
+		fprintf(stderr, "Usage: hhistory path/to/zsh_history\n");
+		return -1;
+	}
+	fp = fopen(argv[1], "r");
+
+	if (fp == NULL) {
+		perror("fopen");
+		return -1;
+	}
+
+	char s[128], *p = s;
+	int last_timestamp = 0, timestamp, n;
+
+	while (true) {
+		int ret = fread(p, 1, 1, fp);
+
+		if (feof(fp)) {
+			break;
+		}
+
+		if (*p == '\n') {
+			*p = '\0';
+
+			if (sscanf(s, ": %d:0;%n", &timestamp, &n) == 1) {
+				if (timestamp >= last_timestamp) {
+					v.push_back(std::make_pair(timestamp, s + n));
+					last_timestamp = timestamp;
+				}
+			}
+
+			p = s;
+		} else if (p - s < (sizeof s) - 1) {
+			p++;
+		}
+	}
+
+	int now = time(0);
+	struct YMDHMS now_date;
+	fast_date(&now_date, now);
+	struct YMDHMS most_old_date;
+	fast_date(&most_old_date, v[0].first);
+	int size = 0;
+
+	for (int i = 1; i <= v.size(); i *= 10) {
+		size++;
+	}
+
+	for (int i = v.size() - 1; i >= 0; i--) {
+		struct YMDHMS timestamp_date;
+		char timestr[32];
+		const char *week;
+		fast_date(&timestamp_date, v[i].first);
+		switch(v[i].first/86400%7){
+			case 0:week="四";break;
+			case 1:week="五";break;
+			case 2:week="六";break;
+			case 3:week="日";break;
+			case 4:week="一";break;
+			case 5:week="二";break;
+			case 6:week="三";break;
+		}
+
+		if (most_old_date.Y != now_date.Y) {
+			if (now_date.Y != timestamp_date.Y) {
+				snprintf(timestr, sizeof timestr, "%04d-%02d-%02d %02d:%02d:%02d",
+				         timestamp_date.Y, timestamp_date.M, timestamp_date.D,
+				         timestamp_date.h, timestamp_date.m, timestamp_date.s);
+			} else if (now_date.M != timestamp_date.M) {
+				snprintf(timestr, sizeof timestr, "     %02d-%02d %02d:%02d:%02d",
+				         timestamp_date.M, timestamp_date.D,
+				         timestamp_date.h, timestamp_date.m, timestamp_date.s);
+			} else if (now_date.D != timestamp_date.D) {
+				snprintf(timestr, sizeof timestr, "        %02d %02d:%02d:%02d",
+				         timestamp_date.D,
+				         timestamp_date.h, timestamp_date.m, timestamp_date.s);
+			} else {
+				snprintf(timestr, sizeof timestr, "           %02d:%02d:%02d",
+				         timestamp_date.h, timestamp_date.m, timestamp_date.s);
+			}
+		} else {
+			if (now_date.M != timestamp_date.M) {
+				snprintf(timestr, sizeof timestr, "%02d-%02d %02d:%02d:%02d",
+				         timestamp_date.M, timestamp_date.D,
+				         timestamp_date.h, timestamp_date.m, timestamp_date.s);
+			} else if (now_date.D != timestamp_date.D) {
+				snprintf(timestr, sizeof timestr, "   %02d %02d:%02d:%02d",
+				         timestamp_date.D,
+				         timestamp_date.h, timestamp_date.m, timestamp_date.s);
+			} else {
+				snprintf(timestr, sizeof timestr, "      %02d:%02d:%02d",
+				         timestamp_date.h, timestamp_date.m, timestamp_date.s);
+			}
+		}
+
+		char fmt[64];
+		snprintf(fmt, sizeof fmt, "%%%dd %%s %%s %%s\n", size);
+		printf(fmt, i+1, week, timestr, v[i].second.c_str());
+	}
+
+	fclose(fp);
+	return 0;
+}
