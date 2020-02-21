c909d0c5b7813eb5909a0f07da7a1cfa91bcacfa
YouDad <1632083718@qq.com>
2020-02-21 20:23:23 +0000

    UPDATE: git source

--- my/install/zsh_h/install.sh
+++ my/install/zsh_h/install.sh
@@ -6,8 +6,8 @@ else
 	exit 1
 fi
 
-git clone git@github.com:paoloantinori/hhighlighter.git $ZSH_CUSTOM/plugins/h
-check_retval "git clone git@github.com:paoloantinori/hhighlighter.git $ZSH_CUSTOM/plugins/h"
+git clone https://github.com/paoloantinori/hhighlighter $ZSH_CUSTOM/plugins/h
+check_retval "git clone https://github.com/paoloantinori/hhighlighter $ZSH_CUSTOM/plugins/h"
 
 mv $ZSH_CUSTOM/plugins/h/h.sh $ZSH_CUSTOM/plugins/h/h.plugin.zsh
 check_retval "mv $ZSH_CUSTOM/plugins/h/h.sh $ZSH_CUSTOM/plugins/h/h.plugin.zsh"
