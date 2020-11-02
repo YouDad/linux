f28d02267e3fc136ebdb8badb54915ca929a0b91
YouDad <1632083718@qq.com>
2020-11-02 13:33:20 +0800

    UPDATE[bin]: update xrandr shell name

--- bin/xrandr-thundersoft-1.sh
+++ bin/xrandr-thundersoft-1.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
 # 内置屏 15.6'
-PRIName=eDP1
+PRIName=DP3
 PRIMode=1920x1080
 PRIPos=1920x0
 PRIRate=60
 
 xrandr \
-	--output HDMI1 --off \
-	--output HDMI2 --off \
-	--output VIRTUAL1 --off \
-	--output DP1 --off \
 	--output $PRIName --mode $PRIMode --pos $PRIPos --rate $PRIRate --primary \
--- bin/xrandr-thundersoft-2.sh
+++ bin/xrandr-thundersoft-2.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-# 外接屏
-PRIName=DP1
+PRIName=DP3
 PRIMode=1920x1080
 PRIPos=0x0
-PRIRate=144
+PRIRate=60
 
-# 内置屏 15.6'
-SECName=eDP1
+SECName=DP1-8
 SECMode=1920x1080
-SECPos=0x0
+SECPos=1920x0
 SECRate=60
 
 xrandr \
-	--output HDMI1 --off \
-	--output HDMI2 --off \
-	--output VIRTUAL1 --off \
+	--output $PRIName --mode $PRIMode --pos $PRIPos --rate $PRIRate \
+	--output $SECName --mode $SECMode --pos $SECPos --rate $SECRate --primary
+
+xrandr \
 	--output $PRIName --mode $PRIMode --pos $PRIPos --rate $PRIRate --primary \
 	--output $SECName --mode $SECMode --pos $SECPos --rate $SECRate
